6b4a155c6d6be9ef9cedba1ba5058051094e63df
jmblalock <justin.m.blalock@gmail.com>
2021-02-16 10:22:38 -0600

    destructuring-examples 2

--- destructuring-examples.js
+++ destructuring-examples.js
@@ -31,11 +31,54 @@ console.log(`l = ${l}`);
 // === Objects
 var { user: x } = {user: 5}
 console.log(x);
+// => 5
 
 // Fail-safe
 var { user1: y } = { user2: 5 };
 console.log(y);
+// => undefined
 
 // More values
 var { prop1: x, prop2: y} = { prop1: 5, prop2: 10 };
 console.log(x, y);
+// => 5 10
+
+// Short-hand syntax
+var { prop1, prop2 } = { prop1: 5, prop2: 10 }
+console.log(prop1, prop2);
+// => 5 10
+
+// Equal to:
+var { prop1: prop1, prop2: prop2 } = { prop1: 5, prop2: 10 }
+console.log(prop1, prop2);
+// => 5 10
+
+// === Potential grammar hiccups
+
+// Oops: This doesn't work:
+// var a, b;
+// { a, b } = { a: 1, b: 2 };
+
+// But this does work
+var m, n;
+({ m, n } = { m: 1, n: 2 });
+console.log(a, b);
+// => 1 2
+
+// This is due to the grammar in JS.
+// Starting with { implies a block scope, not an object literal.
+// () converts to an expression.
+
+// From Harmony Wiki:
+// Note that object literals cannot appear in
+// statement position, so a plain object
+// destructuring assignment statment
+// { x } = y must be parenthesized either
+// as ({ x } = y) or ({ x }) = y
+
+// === Combined destructuring of objects and arrays
+
+// Combine objects and arrarys
+var { prop1: x, prop2: [ , y] } = { prop1: 5, prop2: [10, 100]};
+console.log(x, y);
+// => 5 100
