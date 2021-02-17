9bcb1ebf9f5ced15395ef1bb5ef5fc37b3601128
jmblalock <justin.m.blalock@gmail.com>
2021-02-16 18:35:22 -0600

    looping-exercise-part-3

--- looping-exercise.js
+++ looping-exercise.js
@@ -10,6 +10,8 @@ const game = {
   ]
 };
 
+// Exercise I
+
 // for (let i = 0; i < game.suspects.length; i++) {
 //   console.log(game.suspects[i]);
 // }
@@ -18,11 +20,24 @@ const game = {
 //   console.log(game[key]);
 // }
 
-for (let i = 0; i < game.suspects.length; i++) {
-  // console.log([i]);
-  for (key in game.suspects[i]) {
-    // console.log(key);
-    console.log(game.suspects[i]);
-    // console.log(game.suspects[i][key]);
-  }
-}
+// Exercise II
+
+// for (let i = 0; i < game.suspects.length; i++) {
+//   // console.log([i]);
+//   for (key in game.suspects[i]) {
+//     // console.log(key);
+//     // console.log(game.suspects[i]);
+//     console.log(game.suspects[i][key]);
+//   }
+// }
+
+// Exercise III
+
+var [ color1, color2 ] = [ game.suspects[0].color, game.suspects[1].color ];
+
+console.log(color1, color2);
+
+// var [ firstColor, secondColor ] = game.suspects;
+var [{ color: firstColor}, { color: secondColor}] = game.suspects; // more specific into the nested object
+
+console.log(firstColor, secondColor);
