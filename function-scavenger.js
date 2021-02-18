210fd839540eff362d7eb92a7a2b38bfe03744c6
jmblalock <justin.m.blalock@gmail.com>
2021-02-17 18:29:59 -0600

    function-scavenger

---
+++ function-scavenger.js
@@ -0,0 +1,92 @@
+var nameImprover = function (name, adj) {
+    return 'Col ' + name + ' Mc' + adj + ' pants';
+};
+
+$('body').hide();
+
+myArr.forEach(function(val){ console.log(val);});
+
+$('button').on('click', function(){ 
+console.log('Don\'t press my buttons!');
+});
+
+// Function Definitions:
+// -------------------------->
+// function (name, adj) {
+//     return 'Col ' + name + ' Mc' + adj + ' pants';
+// };
+
+// function(val){ console.log(val);}
+
+// function() { 
+//     console.log('Don\'t press my buttons!');
+// }
+
+// Function Names:
+// ---------------------------------------------------->
+// nameImprover, hide, forEach, on, log, $
+
+
+// Function Bodies:
+// ---------------------------------------------------->
+// {
+//     return 'Col ' + name + ' Mc' + adj + ' pants';
+// };
+
+// { console.log(val);}
+
+// { 
+//     console.log('Don\'t press my buttons!');
+// }
+
+// Function Invocation / Call-Time
+// ---------------------------------------------------->
+
+// log(val)
+
+// log('Don\'t press my buttons!')
+
+// $('body')
+
+// hide()
+
+// forEach(function(val){ console.log(val);})
+
+// $('button')
+
+// on('click', function(){ 
+// console.log('Don\'t press my buttons!');
+// })
+
+// Function Parameters
+// ---------------------------------------------------->
+
+// name, adj
+
+// val
+
+// Function Arguments
+// ---------------------------------------------------->
+
+// 'body'
+
+// 'button'
+
+// 'click'
+
+// function(){ 
+//     console.log('Don\'t press my buttons!');
+// }
+
+// function(val){ console.log(val);}
+
+// 'Don\'t press my buttons!'
+
+// Function Return / Side Effects
+// ---------------------------------------------------->
+
+// return 'Col ' + name + ' Mc' + adj + ' pants';
+
+// console.log(val);
+
+// $('body').hide()
